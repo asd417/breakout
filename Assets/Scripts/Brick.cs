13f2916fd0a5332095e97d23ca1546df724de531
asd417 <loctitematt417@gmail.com>
2024-09-11 17:33:21 -0400

    colors

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    int health = 1;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision != null)
+        {
+            if(collision.transform.CompareTag("ball"))
+            {
+                Debug.Log("Ball Collided");
+                health -= 1;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
